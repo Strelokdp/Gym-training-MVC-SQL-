dcb23e8d7b739091748a486e9629cb428f2f2b96
Женя <laeda@list.ru>
2016-10-31 19:23:20 +0300

    Fixed mistakes, removed enum speciality for now

--- Gym_sports_training/Models/Entities/Coach.cs
+++ Gym_sports_training/Models/Entities/Coach.cs
@@ -10,7 +10,6 @@ namespace Gym_sports_training.Models.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public enum Speciality { get; set; }
         public int Price { get; set; }
         public int TrainingLength { get; set; }
         public string Description { get; set; }
