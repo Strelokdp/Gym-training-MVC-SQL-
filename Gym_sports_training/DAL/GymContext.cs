0b22996e4dfc27c119d0fe044eab6101ad292c71
Женя <laeda@list.ru>
2016-11-01 08:48:27 +0300

    Added initiazlier + context

---
+++ Gym_sports_training/DAL/GymContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Gym_sports_training.Models.Entities;
+
+namespace Gym_sports_training.DAL
+{
+    public class GymContext:DbContext
+    {
+        public GymContext(): base ("GymContext")
+        {
+
+        }
+
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<TrainingSession> TrainingSessions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
