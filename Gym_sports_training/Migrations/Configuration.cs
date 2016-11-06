73482b4e25cf3552129424f89567944bb5035f1d
Женя <laeda@list.ru>
2016-11-06 12:20:23 +0300

    Made migrations

---
+++ Gym_sports_training/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Gym_sports_training.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Gym_sports_training.DAL.GymContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Gym_sports_training.DAL.GymContext";
+        }
+
+        protected override void Seed(Gym_sports_training.DAL.GymContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
